e26331dd9ca91b1f317be68a086ffd624623e692
NFerracuti <npferracuti@gmail.com>
2023-05-14 17:50:53 -0400

    created spinner2

---
+++ spinner2.js
@@ -0,0 +1,8 @@
+const spinner = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+
+for (let i = 0; i < spinner.length; i++) {
+
+  setTimeout(() => {
+    process.stdout.write(spinner[i]);
+  }, i * 100);
+};
